241d2dd5fc616fffd1b2883feb02a5149474d6dd
joshib123 <joshib@science.regn.net>
2020-08-15 14:35:34 +0000

    structured model

--- citation_bio_trainer/model/FTLSTM.py
+++ citation_bio_trainer/model/FTLSTM.py
@@ -11,6 +11,8 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 from tensorflow.keras.optimizers import *
 from tensorflow.keras.regularizers import *
+import matplotlib.pyplot as plt
+
 
 def calulate_ser_jer(y_true, y_pred):
     """
@@ -28,53 +30,84 @@ def calulate_ser_jer(y_true, y_pred):
         jer = fn / float(tp + fn)
     return ser, jer
 
-class Metrics(callbacks.Callback):
-    def __init__(self, train_data, validation_data):
-        super(Metrics, self).__init__()
-        self.validation_data = validation_data
-        self.train_data = train_data
+# class Metrics(callbacks.Callback):
+#     def __init__(self, train_data, validation_data):
+#         super(Metrics, self).__init__()
+#         self.validation_data = validation_data
+#         self.train_data = train_data
         
-    def on_train_begin(self, logs={}):
-        self.val_ser = []
-        self.val_jer = []
+#     def on_train_begin(self, logs={}):
+#         self.val_ser = []
+#         self.val_jer = []
         
-    def on_epoch_end(self, epoch, logs):
-        vx = self.validation_data[0]
-        vy = self.validation_data[1]
-        vx = np.array(vx)
-        pred_y = None
-        pred_y = self.model.predict(vx)
-        py = np.argmax(pred_y, axis=-1)
-        #vy = np.argmax(vy, axis=-1)
-        vy = np.array(vy).flatten()
-        py = np.array(py).flatten()
-        ser, jer = calulate_ser_jer(vy, py)
-        print(ser, jer)
-        self.val_ser.append(ser)
-        self.val_jer.append(jer)
-        logs["val_ser"] = ser
-        logs["val_jer"] = jer
-        print(f"— val_ser: {ser} — val_jer: {jer}")
-        return 
+#     def on_epoch_end(self, epoch, logs):
+#         vx = self.validation_data[0]
+#         vy = self.validation_data[1]
+#         vx = np.array(vx)
+#         pred_y = None
+#         pred_y = self.model.predict(vx)
+#         py = np.argmax(pred_y, axis=-1)
+#         #vy = np.argmax(vy, axis=-1)
+#         vy = np.array(vy).flatten()
+#         py = np.array(py).flatten()
+#         ser, jer = calulate_ser_jer(vy, py)
+#         print(ser, jer)
+#         self.val_ser.append(ser)
+#         self.val_jer.append(jer)
+#         logs["val_ser"] = ser
+#         logs["val_jer"] = jer
+#         print(f"— val_ser: {ser} — val_jer: {jer}")
+#         return 
     
     
     
 def get_model(feat_config, model_config, maxlen):
     input = Input(shape=(maxlen,))
-    embed = Embedding(input_dim=feat_config['max_vocab'], input_length=maxlen, output_dim=300, weights=[embedding_matrix], trainable=model_config['trainable'])(input)
+    embed = Embedding(input_dim=feat_config['max_vocab'], input_length=maxlen, output_dim=300, weights=[model_config['embedding']['matrix']], trainable=model_config['trainable'])(input)
     if model_config['lstm']['use']:
         for ind in range(model_config['lstm']['num']):
             if ind ==0:
-                lstm = LSTM(model_config['lstm']['units'], return_sequences=True)(embed)
+                y = LSTM(model_config['lstm']['units'], return_sequences=True)(embed)
             else:
-                lstm = LSTM(model_config['lstm']['units'], return_sequences=True)(lstm)
+                y = LSTM(model_config['lstm']['units'], return_sequences=True)(y)
     
-    aux_feats = Input(shape=(maxlen,26))
-    conc = Concatenate([lstm, aux_feats])
-    td = TimeDistributed(Dense(1, activation=model_config['output_activation']))(lstm)
-    model = Model(inputs=[input, aux_feats], outputs=td)
+    if model_config['aux_feats']['use']:
+        aux_feats = Input(shape=(maxlen,model_config['aux_feats']['dim']))
+        y = Concatenate()([y, aux_feats])
+        
+    if model_config['dense']['use']:
+        y =  Dense(model_config['dense']['units'], activation=model_config['dense']['activation'])(y)
+        
+    if model_config['timedistributed']['use']:
+        y = TimeDistributed(Dense(1, activation=model_config['output_activation']))(y)
+    else:
+        y = Dense(1, activation=model_config['output_activation'])(y)
+        
+    if model_config['aux_feats']['use']:
+        model = Model(inputs=[input, aux_feats], outputs=y)
+    else:
+        model = Model(inputs=input, outputs=y)
+        
     model.compile(optimizer=model_config['optimizer'],
         loss='binary_crossentropy',
         metrics=['accuracy']
     )
     return model
+
+
+def plot_output(history):
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
+    axes[0].plot(history.history['accuracy'])
+    axes[0].plot(history.history['val_accuracy'])
+    axes[0].title.set_text('model accuracy')
+    axes[0].set_ylabel('accuracy')
+    axes[0].set_xlabel('epoch')
+    axes[0].legend(['train', 'val'], loc='upper left')
+
+    axes[1].plot(history.history['loss'])
+    axes[1].plot(history.history['val_loss'])
+    axes[1].title.set_text('model loss')
+    axes[1].set_ylabel('loss')
+    axes[1].set_xlabel('epoch')
+    axes[1].legend(['train', 'val'], loc='upper left')
+    fig.tight_layout()
