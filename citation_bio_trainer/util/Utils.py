c9b5e58204bf5c709eef73533bcf48d17519443e
joshib123 <joshib@science.regn.net>
2020-09-02 15:05:02 +0000

    code for split and merge (for evaluation too)

--- citation_bio_trainer/util/Utils.py
+++ citation_bio_trainer/util/Utils.py
@@ -129,10 +129,10 @@ def evaluate(true_labels, pred_labels):
     result = {}
     result['count'] = len(true_labels)
     result['count_citations'] = np.sum([np.sum(np.array(sent) == 1) for sent in true_labels])
-    #ser_jer = [calulate_ser_jer(i,j) for i,j in zip(true_labels, pred_labels)]
+    ser_jer = [calulate_ser_jer(i,j) for i,j in zip(true_labels, pred_labels)]
     accuracy = [accuracy_score(i,j) for i,j in zip(true_labels, pred_labels)]
-    #result['mean_ser'] = np.mean([i[0] for i in ser_jer])
-    #result['mean_jer'] = np.mean([i[1] for i in ser_jer])
+    result['mean_ser'] = np.mean([i[0] for i in ser_jer])
+    result['mean_jer'] = np.mean([i[1] for i in ser_jer])
     result['mean_acc'] = np.mean(accuracy)
     result['num_mistakes_seq'] = np.sum([i !=j for i,j in zip(true_labels, pred_labels)])
     result['num_mistakes_all'] = np.sum([np.sum(np.array(i) != np.array(j)) for i,j in zip(true_labels, pred_labels)])
